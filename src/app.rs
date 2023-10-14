8653b9ad9de228b5deba32b773b5051c3d82ca57
Shuto Shikama <shuto.shikama@gmail.com>
2023-10-14 10:08:21 +0900

    ウィンドウに描画するフレームワークを構築

--- src/app.rs
+++ src/app.rs
@@ -3,7 +3,11 @@ use winit::{
     event_loop::{ControlFlow, EventLoopBuilder},
 };
 
-use crate::{gfx::GlyphManager, tty::TeletypeManager, window::WindowManager};
+use crate::{
+    gfx::{GlyphManager, Renderer},
+    tty::TeletypeManager,
+    window::WindowManager,
+};
 
 pub struct App;
 
@@ -17,10 +21,14 @@ impl App {
         let instance = wgpu::Instance::default();
 
         let mut window_manager = WindowManager::new();
-        let id = window_manager.create_window(&instance, &event_loop).await;
+        let id = window_manager.create_window(&event_loop).await;
+        let window = window_manager.try_get_window(id.clone()).unwrap();
 
         let mut teletype_manager = TeletypeManager::new();
         let _tty_id = teletype_manager.create_teletype();
+
+        let mut renderer = Renderer::new();
+        renderer.register(id.clone(), &instance, &window).await;
 
         // アルファベットの抽出待ち
         task.await.unwrap();
@@ -28,21 +36,18 @@ impl App {
         event_loop.run(move |event, _, control_flow| {
             *control_flow = ControlFlow::Poll;
 
-            let _queue = window_manager.try_get_queue(id).unwrap();
-
             match event {
                 Event::WindowEvent {
-                    event: WindowEvent::Resized(_size),
+                    event: WindowEvent::Resized(size),
                     ..
                 } => {
-                    let device = window_manager.get_device(id).unwrap();
-                    let _buffer = device.create_buffer(&wgpu::BufferDescriptor {
-                        label: None,
-                        size: 128,
-                        usage: wgpu::BufferUsages::MAP_READ,
-                        mapped_at_creation: true,
-                    });
-                    device.poll(wgpu::Maintain::Wait);
+                    renderer.resize(id.clone(), size.width, size.height);
+
+                    let window = window_manager.try_get_window(id.clone()).unwrap();
+                    window.request_redraw();
+                }
+                Event::RedrawRequested(_) => {
+                    renderer.render(id.clone());
                 }
                 Event::WindowEvent {
                     event: WindowEvent::CloseRequested,
