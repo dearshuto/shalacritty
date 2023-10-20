089436f208f9bfbf1d9cbfb04fd0cf762732d1f9
Shuto Shikama <@gmail.com>
2023-10-21 00:06:22 +0900

    文字の配置クラスを追加

--- src/gfx/mod.rs
+++ src/gfx/mod.rs
@@ -1,5 +1,7 @@
+mod content_plotter;
 mod glyph_manager;
 mod renderer;
 
+pub use content_plotter::{CharacterInfo, ContentPlotter};
 pub use glyph_manager::GlyphManager;
 pub use renderer::Renderer;
