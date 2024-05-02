8e6d8a6755bddda0f13725ca7f8b0279bed876a4
Shuto Shikama <@gmail.com>
2024-05-02 10:14:31 +0900

    グリフテクスチャー構築を隠蔽
    モジュール構成のリファクタリング

--- src/gfx/detail/mod.rs
+++ src/gfx/detail/mod.rs
@@ -1,9 +1,11 @@
 mod background_renderer;
 mod cursor_renderer;
+mod glyph_writer;
 mod scan_buffer_renderer;
 mod text_renderer;
 
 pub use background_renderer::BackgroundRenderer;
 pub use cursor_renderer::CursorRenderer;
+pub use glyph_writer::GlyphWriter;
 pub use scan_buffer_renderer::ScanBufferRenderer;
 pub use text_renderer::TextRenderer;
--- src/gfx/mod.rs
+++ src/gfx/mod.rs
@@ -1,10 +1,8 @@
 mod content_plotter;
 mod detail;
 mod glyph_manager;
-mod glyph_writer;
 mod renderer;
 
 pub use content_plotter::ContentPlotter;
 pub use glyph_manager::GlyphManager;
-pub use glyph_writer::GlyphWriter;
 pub use renderer::{Renderer, RendererUpdateParams};
