13015af45f8b65a65335a71cfe386b305f04b36a
Shuto Shikama <@gmail.com>
2025-01-28 21:41:55 +0900

    [WIP] 配置処理だけを記述して差分検出機能を分離したい

--- term-gfx/src/detail/mod.rs
+++ term-gfx/src/detail/mod.rs
@@ -1,7 +1,9 @@
+mod content_plotter;
 mod font_engine;
 mod glyph_writer;
 mod shader_reflection;
 
+pub use content_plotter::ContentPlotter;
 pub use font_engine::FontEngine;
 pub use glyph_writer::{CharacterData, GlyphImagePatch, GlyphWriter, IGlyphManager};
 pub use shader_reflection::{ResourceType, ShaderReflection};
