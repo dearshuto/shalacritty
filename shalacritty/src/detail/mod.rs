c5cbfa512dbf28d53993e74b38711e04ed6396d9
Shuto Shikama <shuto.shikama@gmail.com>
2025-03-12 23:44:25 +0900

    各種機能のサービスを追加

--- shalacritty/src/detail/mod.rs
+++ shalacritty/src/detail/mod.rs
@@ -1,3 +1,7 @@
+mod content_plot_service;
 mod image_cache_ex;
+mod rendering_service;
 
+pub use content_plot_service::ContentPlotService;
 pub use image_cache_ex::ImageCacheEx;
+pub use rendering_service::RenderingService;
--- shalacritty/src/multiplexers/shell_service.rs
+++ shalacritty/src/multiplexers/shell_service.rs
@@ -1,20 +1,36 @@
-use crate::app::WindowSizeChangedEventArgs;
+use crate::{app::WindowSizeChangedEventArgs, Config};
 
 pub struct ShellService {
+    config_receiver: tokio::sync::mpsc::Receiver<Config>,
     receiver: tokio::sync::mpsc::Receiver<WindowSizeChangedEventArgs>,
 }
 
 impl ShellService {
-    pub fn new(receiver: tokio::sync::mpsc::Receiver<WindowSizeChangedEventArgs>) -> Self {
-        Self { receiver }
+    pub fn new(
+        config_receiver: tokio::sync::mpsc::Receiver<Config>,
+        receiver: tokio::sync::mpsc::Receiver<WindowSizeChangedEventArgs>,
+    ) -> Self {
+        Self {
+            config_receiver,
+            receiver,
+        }
     }
 
-    pub async fn update_async(&mut self) -> Result<(), ()> {
-        let Some(_) = self.receiver.recv().await else {
-            // sender 側がドロップしていた
-            return Err(());
-        };
+    pub async fn serve(mut self) {
+        loop {
+            tokio::select!(
+                Some(config) = self.config_receiver.recv() => self.apply_config(config),
+                Some(args) = self.receiver.recv() => self.apply_window_size(args),
+                else => break,
+            );
+        }
+    }
 
-        Ok(())
+    fn apply_config(&mut self, _config: Config) {
+        //
+    }
+
+    fn apply_window_size(&mut self, _args: WindowSizeChangedEventArgs) {
+        //
     }
 }
