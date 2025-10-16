3f9edb56f02de8ffe447fa1dbee8315c6f16230a
Shuto Shikama <shuto.shikama@gmail.com>
2025-10-16 22:05:36 +0900

    fixup! [WIP] ターミナル第二世代

--- shaka/src/input_handling_service/mod.rs
+++ shaka/src/input_handling_service/mod.rs
@@ -1,67 +1,54 @@
-use std::time::Duration;
-
-use tokio::time::Instant;
 use winit::event::KeyEvent;
 
-use crate::shell_service::SpawnRequest;
-
-pub struct Input {
-    pub id: asura::ShellId,
+pub enum Action {
+    RequestSpawn(asura::Config),
+    SendInput((asura::ShellId, String)),
+    ActivateTab(asura::TabId),
+    ActivateShell(asura::ShellId),
 }
 
 pub struct InputHandlingService {
     input_receiver: tokio::sync::mpsc::Receiver<KeyEvent>,
-    swawn_request_sender: tokio::sync::mpsc::Sender<SpawnRequest>,
-    input_sender: tokio::sync::mpsc::Sender<Input>,
+    action_senders: Vec<tokio::sync::mpsc::Sender<Action>>,
     id: Option<asura::ShellId>,
 }
 
 impl InputHandlingService {
-    pub fn new(
-        input_receiver: tokio::sync::mpsc::Receiver<KeyEvent>,
-    ) -> (
-        Self,
-        tokio::sync::mpsc::Receiver<SpawnRequest>,
-        tokio::sync::mpsc::Receiver<Input>,
-    ) {
-        let (swawn_request_sender, spawn_request_receiver) = tokio::sync::mpsc::channel(8);
-        let (input_sender, receiver) = tokio::sync::mpsc::channel(8);
+    pub fn new(input_receiver: tokio::sync::mpsc::Receiver<KeyEvent>) -> Self {
+        Self {
+            input_receiver,
+            action_senders: Vec::default(),
+            id: None,
+        }
+    }
 
-        (
-            Self {
-                input_receiver,
-                swawn_request_sender,
-                input_sender,
-                id: None,
-            },
-            spawn_request_receiver,
-            receiver,
-        )
+    pub fn listen(&mut self) -> tokio::sync::mpsc::Receiver<Action> {
+        let (sender, receiver) = tokio::sync::mpsc::channel(1);
+        self.action_senders.push(sender);
+        receiver
     }
 
     async fn serve(mut self, mut cancellation_token: renge::CancellationToken) {
         loop {
             tokio::select! {
-            Some(key_event) = self.input_receiver.recv() => {},
+            Some(key_event) = self.input_receiver.recv() => self.handle_input(key_event).await,
             _ = &mut cancellation_token => break,
             else => {},
             }
         }
     }
 
-    async fn request_spawn(&mut self) {
-        let (sender, receiver) = tokio::sync::oneshot::channel();
-        let request = SpawnRequest {
-            config: asura::Config::default(),
-            ack: sender,
-        };
-        self.swawn_request_sender
-            .send(request)
-            .await
-            .unwrap_or_default();
+    async fn handle_input(&mut self, key_event: KeyEvent) {
+        if let Some(text) = key_event.text {
+            if let Some(id) = self.id {}
+        }
+    }
 
-        let id = receiver.await.unwrap();
-        self.id = Some(id);
+    async fn request_spawn(&mut self) {
+        for sender in &self.action_senders {
+            let config = asura::Config::default();
+            sender.send(Action::RequestSpawn(config)).await.unwrap();
+        }
     }
 }
 
