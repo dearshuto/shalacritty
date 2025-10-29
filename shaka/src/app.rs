5175ba13d9d35f3c0554a29f91739dfacb0b0e6c
Shuto Shikama <@gmail.com>
2025-10-29 21:52:12 +0900

    fixup! [WIP] プラグイン構成

---
+++ shaka/src/app.rs
@@ -0,0 +1,41 @@
+use winit::{
+    application::ApplicationHandler,
+    event::WindowEvent,
+    window::{Window, WindowAttributes},
+};
+
+use crate::PluginSystem;
+
+pub struct App {
+    window: Option<Window>,
+}
+
+impl App {
+    pub fn new() -> Self {
+        Self { window: None }
+    }
+}
+
+impl ApplicationHandler for App {
+    fn resumed(&mut self, event_loop: &winit::event_loop::ActiveEventLoop) {
+        let _ = asura::Multiplexer::new();
+
+        PluginSystem::new().execute();
+
+        let window_attributes = WindowAttributes::default();
+        let window = event_loop.create_window(window_attributes).unwrap();
+        self.window = Some(window);
+    }
+
+    fn window_event(
+        &mut self,
+        event_loop: &winit::event_loop::ActiveEventLoop,
+        _window_id: winit::window::WindowId,
+        event: winit::event::WindowEvent,
+    ) {
+        match event {
+            WindowEvent::CloseRequested => event_loop.exit(),
+            _ => {}
+        }
+    }
+}
--- shaka/src/main.rs
+++ shaka/src/main.rs
@@ -1,8 +1,8 @@
-use shaka::PluginSystem;
+use shaka::App;
+use winit::event_loop::EventLoop;
 
 #[tokio::main]
 async fn main() {
-    let _ = asura::Multiplexer::new();
-
-    PluginSystem::new().execute();
+    let event_loop = EventLoop::builder().build().unwrap();
+    event_loop.run_app(&mut App::new()).unwrap();
 }
