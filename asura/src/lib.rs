258fdb54a9e78b4d58787baaf6c43b9d497213b7
Shuto Shikama <@gmail.com>
2026-01-11 15:33:04 +0900

    シェル操作を読み取りと編集で別管理できるように修正

--- asura/src/lib.rs
+++ asura/src/lib.rs
@@ -6,7 +6,7 @@ mod terminal_emulator;
 pub mod util;
 
 pub use detail::{Content, TeletypeId};
-pub use multiplexer::{Config, Event, Multiplexer, ShellController};
+pub use multiplexer::{Config, Event, Multiplexer, ShellController, ShellReceiver, ShellSender};
 pub use shell_id::ShellId;
 pub use tab_id::TabId;
 pub use terminal_emulator::{Diff, DiffContent, DiffContext, DiffType, TerminalEmulator};
