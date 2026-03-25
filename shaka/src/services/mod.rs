4072684ab9264e3ce7244e23d6836ba503e18eec
Shuto Shikama <@gmail.com>
2026-03-25 23:19:33 +0900

    zellij と連携する仕込み

--- shaka/src/services/mod.rs
+++ shaka/src/services/mod.rs
@@ -2,8 +2,10 @@ mod glyph_extract_service;
 mod input_event_service;
 mod rendering_service;
 mod shell_service;
+mod zellij_bridge_service;
 
 pub use glyph_extract_service::GlyphExtractService;
 pub use input_event_service::{Action, InputEventService};
 pub use rendering_service::{RenderingService, RenderingServiceParams};
 pub use shell_service::ShellService;
+pub use zellij_bridge_service::ZellijBridgeService;
