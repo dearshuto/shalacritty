99eef7fba039eb93daf24da508257dcec5007602
Shuto Shikama <@gmail.com>
2026-04-12 22:41:17 +0900

    [WIP] シェルの差分を通知
    TODO: レイアウト遷移がうまく機能せず画面に文字が表示されなくなる

--- shaka/src/services/shell_service/mod.rs
+++ shaka/src/services/shell_service/mod.rs
@@ -1,4 +1,7 @@
+use std::time::Duration;
+
 use renge::Service;
+use tokio::task;
 
 use crate::services::Action;
 
@@ -22,35 +25,46 @@ impl ShellService {
 impl Service for ShellService {
     async fn serve(mut self, mut cancellation_token: renge::CancellationToken) {
         let mut multiplexer = asura::Multiplexer::new();
-        let (_id, mut controller) = multiplexer.spawn(&asura::Config::default());
-        loop {
-            match controller.recv_event() {
-                Ok(event) => match event {
-                    asura::Event::Updated => break,
-                    asura::Event::Exit => return,
-                    asura::Event::Others => continue,
-                },
-                Err(_) => return,
-            }
-        }
+        let (_id, mut shell_sender, shell_receiver) =
+            multiplexer.spawn_separated(&asura::Config::default());
 
-        let content = controller
-            .read_contents()
-            .acquire_contents()
-            .iter()
-            .filter_map(|c| if c.code != ' ' { Some(c.code) } else { None })
-            .collect();
-        self.content_sender.send(content).await.unwrap();
+        let (content_sender, mut content_receiver) = tokio::sync::mpsc::channel(10);
+        let task = task::spawn(async move {
+            loop {
+                tokio::time::sleep(Duration::from_millis(100)).await;
+                match shell_receiver.try_recv_event() {
+                    Ok(event) => match event {
+                        asura::Event::Updated => {
+                            let str: String = shell_receiver
+                                .read_contents()
+                                .acquire_contents()
+                                .iter()
+                                .map(|c| c.code)
+                                .collect();
+                            content_sender.send(str).await.unwrap();
+                        }
+                        asura::Event::Exit => continue,
+                        asura::Event::Others => continue,
+                    },
+                    Err(_) => continue,
+                }
+            }
+        });
 
         loop {
             tokio::select! {
                 Some(action) = self.content_receiver.recv() => {
                     let Action::Input(str) = action else { continue };
-                    controller.send_input(&str);
+                    shell_sender.send_input(&str);
+                },
+                Some(str) = content_receiver.recv() => {
+                    self.content_sender.send(str).await.unwrap();
                 },
                 _ = &mut cancellation_token => break,
-                else => {}
+                else => {println!("else")}
             }
         }
+
+        task.await.unwrap();
     }
 }
