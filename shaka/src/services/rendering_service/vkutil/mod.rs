ae20f6414ccc0db466364869c039f2db8ba194ed
Shuto Shikama <@gmail.com>
2026-07-20 13:59:05 +0900

    物理デバイスの検索処理をファイル分割するリファクタリング

--- shaka/src/services/rendering_service/vkutil/mod.rs
+++ shaka/src/services/rendering_service/vkutil/mod.rs
@@ -1,5 +1,7 @@
 mod application_info;
 mod debug_utils;
+mod physical_device_finder;
 
 pub use application_info::ApplicationInfoFactory;
 pub use debug_utils::DebugUtils;
+pub use physical_device_finder::search_device_capability;
